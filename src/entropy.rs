de45d06a6281da4195a8b3b9f014aad8fcebb0fa
jcasben <jcasben02@gmail.com>
2023-11-13 19:52:39 +0100

    [UPDATE] Added test for entropy.rs

--- src/entropy.rs
+++ src/entropy.rs
@@ -31,3 +31,17 @@ pub fn input_calculate_entropy() {
         Err(e) => eprintln!("\n{}{}", "ERROR: ".red(), e.red())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn calculate_entropy_test() {
+        let probs: Vec<f32> = vec![
+            0.082, 0.015, 0.028, 0.043, 0.124, 0.022, 0.020, 0.061, 0.070, 0.002,
+            0.008, 0.040, 0.024, 0.067, 0.075, 0.019, 0.001, 0.060, 0.063, 0.091,
+            0.028, 0.010, 0.024, 0.002, 0.0200, 0.001
+        ];
+        let entropy = (super::calculate_entropy(probs) * 100000.0).round() / 100000.0;
+        assert_eq!(entropy, 4.18752);
+    }
+}
