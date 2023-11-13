62d7862bb4d29af8e353660f65d119279b8fcb99
jcasben <jcasben02@gmail.com>
2023-11-13 23:10:32 +0100

    [UPDATE] Finished the methods and refactorized the menu.

--- src/efficiencies/flow_control.rs
+++ src/efficiencies/flow_control.rs
@@ -3,19 +3,21 @@ use colored::Colorize;
 use crate::efficiencies::calculate_a;
 use crate::parse_user_input;
 
-/// Shows the
+/// Shows the menu to select which control flow efficiency
+/// we want to calculate.
 pub fn flow_control_menu() {
-    let tab: String = String::from("   ");
-    let mut option: String = String::new();
+    const MENU: &str =
+r#"    ************ CONTROL DE FLUJO ***********
+    *   1 - Stop & Wait                     *
+    *   2 - Ventana Deslizante              *
+    *   v - Volver                          *
+    *****************************************"#;
 
     'main_loop: loop {
+        let mut option: String = String::new();
         println!("\n\n");
-        println!("************ CONTROL DE FLUJO ***********");
-        println!("*{tab}1 - Stop & Wait                     *");
-        println!("*{tab}2 - Ventana Deslizante              *");
-        println!("*{tab}v - Volver                          *");
-        println!("*****************************************");
-        print!("\nEscoge que deseas hacer: ");
+        println!("{}", MENU.bright_cyan());
+        print!("\n{}", "Escoge que deseas hacer: ".bright_cyan());
         io::stdout().flush().unwrap();
 
         io::stdin()
@@ -25,46 +27,58 @@ pub fn flow_control_menu() {
         match option.as_str().trim() {
             "v" => break 'main_loop,
             "1" => input_fc_stop_and_wait(),
-            "2" => println!(),
-            _ => println!(),
+            "2" => input_fc_slippery_window(),
+            _ => eprintln!("\n{}", "ERROR: La opción escogida no existe dentro de las posibles".red()),
         }
     }
 }
 
+/// Calculates the efficiency of a Stop & Wait control flow mechanism.
 ///
-pub fn fc_stop_and_wait(tprop: f32, tframe: f32) -> f32 {
+/// # Arguments
+/// * `tprop` - the value of the relation d/vprop.
+/// * `tframe` - the value of the relation L/R.
+///
+/// # Returns
+/// The value of the efficiency of this mechanism.
+fn fc_stop_and_wait(tprop: f32, tframe: f32) -> f32 {
     1.0 / (1.0 + 2.0 * calculate_a(tprop, tframe))
 }
 
+/// Takes the user input for the required values and calls
+/// to fc_stop_and_wait(). It prints the result if the process
+/// was successful or an error if there was any.
 pub fn input_fc_stop_and_wait() {
-    let mut tprop: f32 = 0.0;
-    let mut tframe: f32 = 0.0;
-    match parse_user_input("Introduce el valor para tprop(d/vprop): ") {
-        Ok(value) => tprop = value,
-        Err(err) => eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red())
-    }
+    let tprop: f32 = match parse_user_input("Introduce el valor para tprop(d/vprop): ") {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red());
+            return;
+        }
+    };
 
-    //If tprop == 0.0 at this point, it is because an error
-    //ocurred.
-    if tprop == 0.0 {
-        return;
-    }
-
-    match parse_user_input("Introduce el valor para tframe(L/R): ") {
-        Ok(value) => tframe = value,
-        Err(err) => eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red())
-    }
-
-    if tframe == 0.0 {
-        return;
-    }
+    let tframe: f32 = match parse_user_input("Introduce el valor para tframe(L/R): ") {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red());
+            return;
+        }
+    };
 
     let efficiency = fc_stop_and_wait(tprop, tframe);
-    println!("{}{}", "Eficiencia s&w = ".blue(), efficiency.to_string().blue());
+    println!("{}{}", "Eficiencia Stop & Wait = ".blue(), efficiency.to_string().blue());
 }
 
+/// Calculates the efficiency of a slippery window control flow mechanism.
 ///
-pub fn fc_slippery_window(tprop: f32, tframe: f32, n: u16) -> f32  {
+/// # Arguments
+/// * `tprop` - the value of the relation d/vprop.
+/// * `tframe` - the value of the relation L/R.
+/// * `n` - the size of the window.
+///
+/// # Returns
+/// The value for the efficiency of this mechanism.
+fn fc_slippery_window(tprop: f32, tframe: f32, n: u16) -> f32  {
     let a: f32 = calculate_a(tprop, tframe);
 
     if n as f32 >= (2.0 * a + 1.0) {
@@ -72,4 +86,36 @@ pub fn fc_slippery_window(tprop: f32, tframe: f32, n: u16) -> f32  {
     }
 
     n as f32 / (1.0 + 2.0 * a)
+}
+
+/// Takes the user input for the required values and calls
+/// to fc_slippery_window(). It prints the result if the process
+/// was successful or an error if there was any.
+pub fn input_fc_slippery_window() {
+    let tprop: f32 = match parse_user_input("Introduce el valor para tprop(d/vprop): ") {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red());
+            return;
+        }
+    };
+
+    let tframe: f32 = match parse_user_input("Introduce el valor para tframe(L/R): ") {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red());
+            return;
+        }
+    };
+
+    let n = match parse_user_input("Introduce el valor para N: ") {
+        Ok(value) => value as u16,
+        Err(err) => {
+            eprintln!("\n{}{}", "ERROR: ".red(), err.to_string().red());
+            return;
+        }
+    };
+
+    let efficiency = fc_slippery_window(tprop, tframe, n);
+    println!("{}{}", "Eficiencia Ventana Deslizante = ".blue(), efficiency.to_string().blue());
 }
