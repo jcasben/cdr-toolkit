a3c3b1e3a9ec7fd285e9d8af987ac0fcf441d948
jcasben <jcasben02@gmail.com>
2023-11-13 23:09:45 +0100

    [UPDATE] Added RustDoc to the method.

--- src/efficiencies.rs
+++ src/efficiencies.rs
@@ -2,7 +2,9 @@ use colored::Colorize;
 
 pub mod flow_control;
 
-pub fn calculate_a(tprop: f32, tframe: f32) -> f32 {
+/// Calculates the value of a, which formula is tprop/tframe.
+/// It is used for all the efficiency calculations.
+fn calculate_a(tprop: f32, tframe: f32) -> f32 {
     let a: f32 = tprop / tframe;
     println!("\n{}{}", "a = ".blue(), a.to_string().blue());
 
